ba1bda42b77b07e8e4c25871e369e9f1971a5c77
root <root@LAPTOP-O5JU2P9K>
2025-02-15 13:22:09 +0530

    change react app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Neeyada Dapparu
+          Neeyada Dapparu but going through feature branch macha
         </p>
         <a
           className="App-link"
